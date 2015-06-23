54db5b42bf951328dbe50314eca3790d1a573f86
MindScape00 <MindScape00@users.noreply.github.com>
2015-06-22 17:55:35 -0700

    ReportMe Main Code (Lua)

---
+++ ReportMe/ReportMe.lua
@@ -0,0 +1,59 @@
+---------- Initialization ----------
+
+--- Setting Variables --- Deprecated, left for Legacy
+
+-- local reportmeloginhandle = CreateFrame("frame","reportmeloginhandle");
+-- reportmeloginhandle:RegisterEvent("PLAYER_LOGIN");
+-- reportmeloginhandle:SetScript("OnEvent", function()
+    -- ReportMeLogin();
+-- end);
+
+-- function ReportMeLogin()
+	-- if reportmemessage == nil then
+	-- reportmemessage = "If you have a problem, report me"
+	-- end
+-- end
+
+--- Defining the "ann" function for later
+
+local function ann(text)
+  SendChatMessage(".ann "..text, "GUILD");
+end
+
+local function test(text) -- A Test Function to test in /say rather than in GLOBAL broadcast.
+  SendChatMessage(""..text, "say");
+end
+
+-- Main Function
+
+function RPRTME_FUNCT(msg, editbox)
+	if reportmemessage == nil or reportmemessage == "" then
+		ann("If you have a problem, report me: http://bit.ly/reportme1");
+	else
+		ann(""..reportmemessage..": http://bit.ly/reportme1");
+	end
+end
+
+function RPRTMEFRAME_OnClick()
+	RPRTME_FUNCT();
+end
+
+--- Slash Command to run
+
+SLASH_RPRTME1, SLASH_RPRTME2 = '/reportme', '/rm'; -- 3.
+function SlashCmdList.RPRTME(msg, editbox) -- 4.
+	RPRTME_FUNCT();
+end
+
+SLASH_RPRTMESET1, SLASH_RPRTMESET2 = '/reportmeset', '/rms';
+function SlashCmdList.RPRTMESET(msg, editbox)
+	if msg == "" then
+		reportmemessage = nil
+		print("ReportMe | Message set to default. ('If you have a problem, report me')")
+	else
+		reportmemessage = msg
+		print("ReportMe | Message Set to '"..msg.."'")
+	end
+end
+
+--- End for now

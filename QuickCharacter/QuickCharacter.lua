5b5498466bc182c5cf03fd0163c44ce48a7ecb2b
MindScape00 <MindScape00@users.noreply.github.com>
2016-05-31 23:17:59 -0700

    Updated to v1.1
    Modified for 4.3.4a Enteleaie, Added /ccqc version check, added /qcrun to manually run the function.

--- QuickCharacter/QuickCharacter.lua
+++ QuickCharacter/QuickCharacter.lua
@@ -21,7 +21,7 @@ function qctitles()
 		for x=1,229 do SendChatMessage(".title add "..x);
 		end 
 		ChatFrame_AddMessageEventFilter("CHAT_MSG_SYSTEM", function(self,event,m,...)
-			if m:find("You have earned the title") or m:find("You have added title") or m:find("You did not specify") then
+			if m:find("Title ") or m:find("You have earned the title") or m:find("Invalid title id") or m:find("Theres is no such command") then
 				return true;
 			end
 		end);
@@ -31,12 +31,30 @@ end
 function qcskills()
 	if CharacterHasBeenSeen == 0 then
 		local s={43,44,45,46,54,55,136,160,172,173,226,228,229,473,415,414,413,293,433};
-		for k,v in pairs(s)do SendChatMessage(".learnsk "..v.."");
+		for k,v in pairs(s)do SendChatMessage(".setskill "..v.." 100");
 		end
 		ChatFrame_AddMessageEventFilter("CHAT_MSG_SYSTEM", function(self,event,m,...)
-			if m:find("Learned skill") or m:find("Your skill") or m:find("You have gained") then
+			if m:find("Skill ") or m:find("Syntax:") or m:find("Set a skill") or m:find("Learned skill") or m:find("Your skill") or m:find("You have gained") then
 				return true;
 			end
 		end);
 	end
 end
+
+--- Slash Command to return current build being used
+
+SLASH_CCQCVERSION1, SLASH_CCQCVERSION2 = '/QuickCharacter', '/ccqc';
+function SlashCmdList.CCQCVERSION(msg, editbox)
+ print("ConvenientCommands: QuickCharacter ReleaseBuild -> 1.1");
+ print("   Manually run all skills and titles: /qcrun");
+end
+
+SLASH_CCQCRUN1 = '/qcrun';
+function SlashCmdList.CCQCRUN(msg, editbox)
+ print("Manually Running QuickCharacter | Learning All Skills and Titles");
+ CharacterHasBeenSeen = 0
+ SayOnce = 0
+ QCInitialize()
+end
+
+--- End for now

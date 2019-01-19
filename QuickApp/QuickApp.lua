2c6c823c1c07200cb32d22a3892fc9587c6c9c19
MindScape00 <MindScape00@users.noreply.github.com>
2019-01-18 20:25:05 -0700

    Updated to v4.0.1 + Changelog
    This brings the addon up-to-date with the latest release version for Legion / Epsilon's version of Enteleaie.

--- QuickApp/QuickApp.lua
+++ QuickApp/QuickApp.lua
@@ -1,65 +1,103 @@
---- Setting Variables
+-------------------------------------------------------------------------------
+-- QuickApp by MindScape - Designed for Enteleaie
+-------------------------------------------------------------------------------
 
-local APPEAR_BUTTON = "|cff00E5EEAppear"
-local SUMMON_BUTTON = "|cff00E5EESummon"
-local INFO_BUTTON = "|cff00E5EEPlayer Info"
+local currentVersion = GetAddOnMetadata("QuickApp", "Version")
 
---- Defining the CMD function for later
+-------------------------------------------------------------------------------
+-- Local Simple Functions
+-------------------------------------------------------------------------------
 
 local function cmd(text)
   SendChatMessage("."..text, "GUILD");
 end
 
---- Placing the buttons into the Right-Click Unit Popup Menu, just below the "Whisper" button.
+-------------------------------------------------------------------------------
+-- Hook to the Context Menu and add in our own items
+-------------------------------------------------------------------------------
 
-UnitPopupButtons["APPEAR_BUTTON"] = { text = APPEAR_BUTTON, dist = 0, tooltipText = "Appear to the selected player."}
-UnitPopupButtons["SUMMON_BUTTON"] = { text = SUMMON_BUTTON, dist = 0, tooltipText = "Summon the selected player to you."}
-UnitPopupButtons["INFO_BUTTON"] = { text = INFO_BUTTON, dist = 0, tooltipText = "Show the selected player's account info and character list."}
+UnitPopupButtons["ConvenientCommandsTitle"] = {text = "ConvenientCommands", dist = 0, isTitle = true, isUninteractable = true, isSubsectionTitle = true};
+UnitPopupButtons["QuickAppAppearButton"] = { text = "|cff00E5EEAppear", dist = 0, tooltipText = "Appear to the selected player."}
+UnitPopupButtons["QuickAppSummonButton"] = { text = "|cff00E5EESummon", dist = 0, tooltipText = "Summon the selected player to you."}
+UnitPopupButtons["QuickAppInfoButton"] = { text = "|cff00E5EEPlayer Info", dist = 0, tooltipText = "Show the selected player's account info and character list."} --Admin Only now - Disabled
 
-local function BuildPopupMenu( addSummon )
+for menu, items in pairs(UnitPopupMenus) do
+	local insertIndex
+	for index, item in ipairs(items) do
+		if item == "ConvenientCommandsTitle" then
+			insertIndex = index
+			if insertIndex then
+				table.insert(items, insertIndex + 1, "QuickAppAppearButton")
+				table.insert(items, insertIndex + 2, "QuickAppSummonButton")
+				break
+			end
+		elseif item == "INTERACT_SUBSECTION_TITLE" then
+			insertIndex = index
+			if insertIndex then
+				table.insert(items, insertIndex + 1 - 1, "ConvenientCommandsTitle")
+				table.insert(items, insertIndex + 2 - 1, "QuickAppAppearButton")
+				table.insert(items, insertIndex + 3 - 1, "QuickAppSummonButton")
+				break
+			end
+		end
+	end
+end
+
+--[[ The Old Code
+--local function BuildPopupMenu( addSummon ) -- Disabled to disable the libEnteleaie Check
   for menu, items in pairs(UnitPopupMenus) do
     for i = 0, #items do
       if items[i] == "WHISPER" or items[i] == "QTWHISPER_BUTTON" then
 		if items[i+1] == "APPEAR_BUTTON" then return; end
         table.insert(items, i + 1, "APPEAR_BUTTON")
-        if addSummon == "1" then
-          table.insert(items, i + 2, "SUMMON_BUTTON")
-          table.insert(items, i + 3, "INFO_BUTTON")
-        end
+		table.insert(items, i + 2, "SUMMON_BUTTON")
+        --if addSummon == "1" then -- Disabled as the Permission Check is no longer Required
+          --table.insert(items, i + 2, "SUMMON_BUTTON") -- This was moved to all players, no longer needs a perm check.
+          --table.insert(items, i + 3, "INFO_BUTTON") -- Admin Only, Removed. Permissions No Longer Needed
+        --end
         break
       end
     end
   end
-end
+--end
+  ]]--
 
----Checking Permissions set of the user
+-------------------------------------------------------------------------------
+-- OnLogin event to check permissions and then build the popupmenu -- Disabled cuz no more Perm Checks
+-------------------------------------------------------------------------------
 
+--[[
 local frame = CreateFrame("frame");
 frame:RegisterEvent("PLAYER_LOGIN");
 frame:SetScript("OnEvent", function()
     libEnteleaie:Get("permissions", "d", BuildPopupMenu);
 end);
+]]--
 
---New, Streamlined OnClick code
+-------------------------------------------------------------------------------
+-- New, Streamlined OnClick code
+-------------------------------------------------------------------------------
 
 local function CCButtons_OnClick(self)
 	local unitpopupframe = UIDROPDOWNMENU_INIT_MENU;
-	if "APPEAR_BUTTON" == self.value then
+	if "QuickAppAppearButton" == self.value then
 	cmd("appear "..unitpopupframe.name)
-	elseif "SUMMON_BUTTON" == self.value then
+	elseif "QuickAppSummonButton" == self.value then
 	cmd("summon "..unitpopupframe.name)
-	elseif "INFO_BUTTON" == self.value then
+	elseif "QuickAppInfoButton" == self.value then
 	cmd("player "..unitpopupframe.name)
 	end
 end
 
 hooksecurefunc("UnitPopup_OnClick", CCButtons_OnClick)
 
---- Slash Command to return current build being used
+-------------------------------------------------------------------------------
+-- Slash Command to return current build being used
+-------------------------------------------------------------------------------
 
 SLASH_CCQAVERSION1, SLASH_CCQAVERSION2 = '/QuickApp', '/ccqa'; -- 3.
 function SlashCmdList.CCQAVERSION(msg, editbox) -- 4.
- print("ConvenientCommands: QuickApp ReleaseBuild -> 3.3");
+ print("|cff00E5EEConvenientCommands: QuickApp v"..currentVersion);
 end
 
 --- End for now
